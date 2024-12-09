df47e5f6e86961509e0f34761c7fdec54dd4f398
zhaogang <zhaogang01@agree.com.cn>
2024-12-09 17:11:50 +0800

    update podspec

---
+++ FadeInFadeOutCPOC.framework/Headers/FadeInFadeOutCPOC.h
@@ -0,0 +1,18 @@
+//
+//  FadeInFadeOutCPOC.h
+//  FadeInFadeOutCPOC
+//
+//  Created by 赵公子 on 2024/12/9.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FadeInFadeOutCPOC.
+FOUNDATION_EXPORT double FadeInFadeOutCPOCVersionNumber;
+
+//! Project version string for FadeInFadeOutCPOC.
+FOUNDATION_EXPORT const unsigned char FadeInFadeOutCPOCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FadeInFadeOutCPOC/PublicHeader.h>
+#import <FadeInFadeOutCPOC/ToolsInterface.h>
+
